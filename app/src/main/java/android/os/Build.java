dab4ad8df0b95a36b979c34373af61b952e75894
zhixc <93297256+zhixc@users.noreply.github.com>
2024-01-24 14:10:03 +0000

    add Build.java

---
+++ app/src/main/java/android/os/Build.java
@@ -0,0 +1,12 @@
+package android.os;
+
+public class Build {
+    public static class VERSION {
+//        public static final int SDK_INT = SystemProperties.getInt("ro.build.version.sdk", 0);
+        public static final int SDK_INT = 0;
+    }
+
+    public static class VERSION_CODES {
+        public static final int LOLLIPOP = 21;
+    }
+}
