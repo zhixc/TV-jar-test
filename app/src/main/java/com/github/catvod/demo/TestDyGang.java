c133c476937c00ad41779607e52dab86ac805ad7
zhixc <93297256+zhixc@users.noreply.github.com>
2024-04-07 10:54:47 +0800

    update DyGang、Xunlei8

--- app/src/main/java/com/github/catvod/demo/TestDyGang.java
+++ app/src/main/java/com/github/catvod/demo/TestDyGang.java
@@ -43,7 +43,8 @@ public class TestDyGang {
         List<String> ids = new ArrayList<>();
 //        ids.add("/ys/20240402/54327.htm");
 //        ids.add("/dmq/20211031/48089.htm");
-        ids.add("/ys/20240402/54324.htm");
+//        ids.add("/ys/20240402/54324.htm");
+        ids.add("/ys/20240403/54333.htm");
         System.out.println(dyGang.detailContent(ids));
     }
 
